29c22c6463e6206075922fc1b832c1166dd106bf
ThomasHeim11 <wearedreamin@gmail.com>
2023-05-15 19:26:53 +0200

    Updated documentation for smart contract👨💻

--- web3/contracts/CrowdFunding.sol
+++ web3/contracts/CrowdFunding.sol
@@ -1,6 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+/**
+ * @title CrowdFunding
+ * @author Thomas Heim
+ * @notice A smart contract for managing crowdfunding campaigns.
+ * @dev This contract allows users to create and donate to crowdfunding campaigns.
+ * It stores information about the campaign, including the owner, title, description,
+ * target amount, deadline, amount collected so far, and image.
+ */
+
 contract CrowdFunding {
     struct Campaign {
         address owner;
@@ -18,10 +27,30 @@ contract CrowdFunding {
 
     uint256 public numberOfCampaigns = 0;
 
-    function createCampaign(address _owner, string memory _title, string memory _description, uint256 _target, uint256 _deadline, string memory _image) public returns (uint256) {
+    /**
+     * @notice Creates a new crowdfunding campaign.
+     * @param _owner The address of the campaign owner.
+     * @param _title The title of the campaign.
+     * @param _description The description of the campaign.
+     * @param _target The target amount to be raised.
+     * @param _deadline The deadline for the campaign.
+     * @param _image The image associated with the campaign.
+     * @return The ID of the newly created campaign.
+     */
+    function createCampaign(
+        address _owner,
+        string memory _title,
+        string memory _description,
+        uint256 _target,
+        uint256 _deadline,
+        string memory _image
+    ) public returns (uint256) {
         Campaign storage campaign = campaigns[numberOfCampaigns];
 
-        require(campaign.deadline < block.timestamp, "The deadline should be a date in the future.");
+        require(
+            campaign.deadline < block.timestamp,
+            "The deadline should be a date in the future."
+        );
 
         campaign.owner = _owner;
         campaign.title = _title;
@@ -36,6 +65,10 @@ contract CrowdFunding {
         return numberOfCampaigns - 1;
     }
 
+    /**
+     * @notice Donates to a crowdfunding campaign.
+     * @param _id The ID of the campaign to donate to.
+     */
     function donateToCampaign(uint256 _id) public payable {
         uint256 amount = msg.value;
 
@@ -44,21 +77,32 @@ contract CrowdFunding {
         campaign.donators.push(msg.sender);
         campaign.donations.push(amount);
 
-        (bool sent,) = payable(campaign.owner).call{value: amount}("");
+        (bool sent, ) = payable(campaign.owner).call{value: amount}("");
 
-        if(sent) {
+        if (sent) {
             campaign.amountCollected = campaign.amountCollected + amount;
         }
     }
 
-    function getDonators(uint256 _id) view public returns (address[] memory, uint256[] memory) {
+    /**
+     * @notice Gets the list of donators and their respective donations for a campaign.
+     * @param _id The ID of the campaign to get the list for.
+     * @return The list of donators and their respective donations.
+     */
+    function getDonators(
+        uint256 _id
+    ) public view returns (address[] memory, uint256[] memory) {
         return (campaigns[_id].donators, campaigns[_id].donations);
     }
 
+    /**
+     * @notice Gets the list of all crowdfunding campaigns.
+     * @return The list of all crowdfunding campaigns.
+     */
     function getCampaigns() public view returns (Campaign[] memory) {
         Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
 
-        for(uint i = 0; i < numberOfCampaigns; i++) {
+        for (uint i = 0; i < numberOfCampaigns; i++) {
             Campaign storage item = campaigns[i];
 
             allCampaigns[i] = item;
