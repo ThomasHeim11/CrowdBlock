d1aec63171586a69bf9fd7bd93b165637b988799
ThomasHeim11 <wearedreamin@gmail.com>
2023-05-26 09:26:26 +0200

    updated build✅

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,10 @@
-import react from "@vitejs/plugin-react";
+// vite.config.js
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  define: {
-    global: "globalThis",
-    "process.env": {},
-    build: {
-      rollupOptions: {
-        external: ["@safe-globalThis/safe-ethers-adapters"],
-      },
+  build: {
+    rollupOptions: {
+      external: ["@safe-globalThis/safe-ethers-adapters"],
     },
   },
 });
